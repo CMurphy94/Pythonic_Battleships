c87dc3d1be84cb5108cdf2dc9a22550ee76e7838
CMurphy94 <conormurphy94@hotmail.com>
2022-12-30 20:16:28 +0000

    Add docstrings and start main game play function

--- run.py
+++ run.py
@@ -4,6 +4,9 @@ BOARD_SIZE = 5
 NUM_OF_SHIPS = 4
 
 def board_setup():
+    """
+    Sets up to board size
+    """
     board = []
     for i in range(BOARD_SIZE):
         row = ["0"] * BOARD_SIZE
@@ -12,20 +15,33 @@ def board_setup():
 
 
 def player_ships_setup():
+    """
+    Sets up the players ship locations
+    """
     player_ships = []
     for i in range(NUM_OF_SHIPS):
         ship_row = random.randint(0, BOARD_SIZE-1)
         ship_col = random.randint(0, BOARD_SIZE-1)
         player_ships.append((ship_row, ship_col))
-    
     print(f" player ships are located at the following locations: {player_ships}")
 
 
+
 def computer_ships_setup():
+    """
+    Sets up the computers ship locations
+    """
     comp_ships = []
     for i in range(NUM_OF_SHIPS):
         ship_row = random.randint(0, BOARD_SIZE-1)
         ship_col = random.randint(0, BOARD_SIZE-1)
         comp_ships.append((ship_row, ship_col))
 
-player_ships_setup()
+
+def gameplay():
+    """
+    Runs the game
+    """
+    while True:
+        x = int(input("Enter the column you'd like to guess: "))
+        y = int(input("Enter the row you'd like to guess: "))
