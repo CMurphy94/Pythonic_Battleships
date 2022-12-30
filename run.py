a242537da01582c38cc78f7e57c5e75094484b31
CMurphy94 <conormurphy94@hotmail.com>
2022-12-30 18:17:42 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,2 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+BOARD_SIZE = 5
+NUM_OF_SHIPS = 4
