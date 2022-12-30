c189c902ea68ee9d85a45a9b90e1727d9d83de93
CMurphy94 <conormurphy94@hotmail.com>
2022-12-30 19:10:37 +0000

    Set up placement of player and computer ship locations

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import random
+
 BOARD_SIZE = 5
 NUM_OF_SHIPS = 4
 
@@ -6,3 +8,24 @@ def board_setup():
     for i in range(BOARD_SIZE):
         row = ["0"] * 5
         board.append(row)
+
+
+
+def player_ships_setup():
+    player_ships = []
+    for i in range(NUM_OF_SHIPS):
+        ship_row = random.randint(0, BOARD_SIZE-1)
+        ship_col = random.randint(0, BOARD_SIZE-1)
+        player_ships.append((ship_row, ship_col))
+    
+    print(f" player ships are located at the following locations: {player_ships}")
+
+
+def computer_ships_setup():
+    comp_ships = []
+    for i in range(NUM_OF_SHIPS):
+        ship_row = random.randint(0, BOARD_SIZE-1)
+        ship_col = random.randint(0, BOARD_SIZE-1)
+        comp_ships.append((ship_row, ship_col))
+
+player_ships_setup()
